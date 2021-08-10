4c77e25b560fbe4021e9ef6387b82afd7325c23b
wayne <wayne71112@gmail.com>
2021-08-11 01:12:44 +0800

    navbar bug 2nd try

--- navbar.js
+++ navbar.js
@@ -1,41 +1,44 @@
-document.write( '<nav class=\"navbar navbar-expand-lg navbar-light bg-light\">\n' );
-document.write( '  <div class=\"container-fluid\">\n' );
-document.write( '    <!--ICON & 標題-->\n' );
-document.write( '    <a class=\"navbar-brand\" href=\"#\">\n' );
-document.write( '      <img src=\"https://i.imgur.com/73cBzrL.png\" alt=\"\" width=\"24\" height=\"24\" class=\"d-inline-block align-text-top\">\n' );
-document.write( '      Yuto\'s Note\n' );
-document.write( '    </a><!--ICON & 標題結束-->\n' );
-document.write( '    <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n' );
-document.write( '      <span class=\"navbar-toggler-icon\"></span>\n' );
-document.write( '    </button>\n' );
-document.write( '    <!--其他頁面-->\n' );
-document.write( '    <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\n' );
-document.write( '      <ul class=\"navbar-nav me-auto mb-2 mb-lg-0\">\n' );
-document.write( '        <li class=\"nav-item\">\n' );
-document.write( '          <a class=\"nav-link active\" aria-current=\"page\" href=\"index.html\">首頁</a>\n' );
-document.write( '        </li>\n' );
-document.write( '        <li class=\"nav-item\">\n' );
-document.write( '          <a class=\"nav-link\" href=\"content.html\">文章</a>\n' );
-document.write( '        </li>\n' );
-document.write( '        <li class=\"nav-item dropdown\">\n' );
-document.write( '          <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">\n' );
-document.write( '            作品集\n' );
-document.write( '          </a>\n' );
-document.write( '          <ul class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">\n' );
-document.write( '            <li><a class=\"dropdown-item\" href=\"note.html\">Js備忘錄</a></li>\n' );
-document.write( '          </ul>\n' );
-document.write( '        </li><!--下拉選單結束-->\n' );
-document.write( '        <!--下拉選單-->\n' );
-document.write( '        <li class=\"nav-item dropdown\">\n' );
-document.write( '          <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">\n' );
-document.write( '            更多\n' );
-document.write( '          </a>\n' );
-document.write( '          <ul class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">\n' );
-document.write( '            <li><a class=\"dropdown-item\" href=\"login.html\">登入</a></li>\n' );
-document.write( '            <li><a class=\"dropdown-item\" href=\"download.html\">下載</a></li>\n' );
-document.write( '          </ul>\n' );
-document.write( '        </li><!--下拉選單結束-->\n' );
-document.write( '      </ul>\n' );
-document.write( '    </div><!-- 其他頁面結束 -->\n' );
-document.write( '  </div>\n' );
-document.write( '</nav>\n' );
+navbar=`
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <!--ICON & 標題-->
+    <a class="navbar-brand" href="#">
+      <img src="https://i.imgur.com/73cBzrL.png" alt="" width="24" height="24" class="d-inline-block align-text-top">
+      Yuto's Note
+    </a><!--ICON & 標題結束-->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!--其他頁面-->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.html">首頁</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="content.html">文章</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            作品集
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="note.html">Js備忘錄</a></li>
+          </ul>
+        </li><!--下拉選單結束-->
+        <!--下拉選單-->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            更多
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="login.html">登入</a></li>
+            <li><a class="dropdown-item" href="download.html">下載</a></li>
+          </ul>
+        </li><!--下拉選單結束-->
+      </ul>
+    </div><!-- 其他頁面結束 -->
+  </div>
+</nav>
+`
+document.write(navbar);
